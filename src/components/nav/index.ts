bd238a344d97272cddd24bd4b1349e89dcde55a2
PaisanDev <ratchanon.pais@gmail.com>
2024-11-15 12:19:22 +0700

    export nav sub components

--- src/components/nav/index.ts
+++ src/components/nav/index.ts
@@ -1,1 +1,5 @@
 export { default as Nav } from "./Nav";
+export { default as NavLogo } from "./Nav";
+export { default as NavLinks } from "./Nav";
+export { default as NavActions } from "./Nav";
+export { default as NavMenus } from "./Nav";
