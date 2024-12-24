33ae54c0097014d54c50d6815ed93df2fa8906b5
PaisanDev <ratchanon.pais@gmail.com>
2024-12-24 11:02:00 +0700

    fix: change test to .tsx and re write test

--- packages/keyDisplayer/src/components/keyDisplayer/__test__/keyDisplayer.test.tsx
+++ packages/keyDisplayer/src/components/keyDisplayer/__test__/keyDisplayer.test.tsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { KeyDisplayer } from "../keyDisplayer";
 
 describe("KeyDisplayer component", () => {
   it("KeyDisplayer should render correctly", () => {
-    render(<KeyDisplayer/>);
+    const mockValue = "your-key-value";
+    const mockOnClick = () => {};
+
+    render(<KeyDisplayer value={mockValue} onClick={mockOnClick} size="medium" />);
     const keyDisplayerElement = screen.getByText("•".repeat(12));
     expect(keyDisplayerElement).toBeInTheDocument();
   });
